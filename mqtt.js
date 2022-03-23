c0e9b26f6cef58dee6157c9dfa9243c4c36d0e4f
Missath <87136961+Missath@users.noreply.github.com>
2022-03-23 15:23:42 -0600

    Js code and renamed file

---
+++ mqtt.js
@@ -0,0 +1,100 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js"
+    type="text/javascript"></script>
+
+
+function onConnectionLost() {
+    console.log("connection lost");
+    document.getElementById("status").innerHTML = "Connection Lost";
+    document.getElementById("messages").innerHTML = "Connection Lost";
+    connected_flag = 0;
+}
+function onFailure(message) {
+    console.log("Failed");
+    document.getElementById("messages").innerHTML = "Connection Failed- Retrying";
+    setTimeout(MQTTconnect, reconnectTimeout);
+}
+function onMessageArrived(r_message) {
+    out_msg = "Message received " + r_message.payloadString + "<br>";
+    out_msg = out_msg + "Message received Topic " + r_message.destinationName;
+    console.log(out_msg);
+    document.getElementById("messages").innerHTML = out_msg;
+}
+
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    document.getElementById("messages").innerHTML = "Connected to " + host + "on port " + port;
+    connected_flag = 1
+    document.getElementById("status").innerHTML = "Connected";
+    console.log("on Connect " + connected_flag);
+    
+}
+
+function MQTTconnect() {
+    document.getElementById("messages").innerHTML = "";
+    var s = document.forms["connform"]["server"].value;
+    var p = document.forms["connform"]["port"].value;
+    if (p != "") {
+        console.log("ports");
+        port = parseInt(p);
+        console.log("port" + port);
+    }
+    if (s != "") {
+        host = s;
+        console.log("host");
+    }
+    console.log("connecting to " + host + " " + port);
+    var x = Math.floor(Math.random() * 10000);
+    var cname = "orderform-" + x;
+    mqtt = new Paho.MQTT.Client(host, port, cname);
+    
+    var options = {
+        timeout: 3,
+        onSuccess: onConnect,
+        onFailure: onFailure,
+
+    };
+
+    mqtt.onConnectionLost = onConnectionLost;
+    mqtt.onMessageArrived = onMessageArrived;
+   
+
+    mqtt.connect(options);
+    return false;
+
+
+}
+function sub_topics() {
+    document.getElementById("messages").innerHTML = "";
+    if (connected_flag == 0) {
+        out_msg = "<b>Not Connected so can't subscribe</b>"
+        console.log(out_msg);
+        document.getElementById("messages").innerHTML = out_msg;
+        return false;
+    }
+    var stopic = document.forms["subs"]["Stopic"].value;
+    console.log("Subscribing to topic =" + stopic);
+    mqtt.subscribe(stopic);
+    return false;
+}
+function send_message() {
+    document.getElementById("messages").innerHTML = "";
+    if (connected_flag == 0) {
+        out_msg = "<b>Not Connected so can't send</b>"
+        console.log(out_msg);
+        document.getElementById("messages").innerHTML = out_msg;
+        return false;
+    }
+    var msg = document.forms["smessage"]["message"].value;
+    console.log(msg);
+
+    var topic = document.forms["smessage"]["Ptopic"].value;
+    message = new Paho.MQTT.Message(msg);
+    if (topic == "")
+        message.destinationName = "test-topic"
+    else
+        message.destinationName = topic;
+    mqtt.send(message);
+    return false;
+}
+
+
